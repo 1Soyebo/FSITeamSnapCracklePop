522cc083901b69a31b307792fa1d49dbdc2d5153
1Soyebo <soyeboibukun@gmail.com>
2020-09-28 19:13:34 +0100

    first commit

---
+++ Loanus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loanus
+//
+//  Created by Ibukunoluwa Soyebo on 28/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Loanus/Pods-Loanus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Loanus : NSObject
+@end
+@implementation PodsDummy_Pods_Loanus
+@end
